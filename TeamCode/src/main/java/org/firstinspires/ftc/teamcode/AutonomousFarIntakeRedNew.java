76315faf7854c06f0a1429200a26c593a6629ae9
Ethan Schaffer <ethan@schaffer.com>
2018-03-26 17:09:57 -0500

    More small tweaks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFarIntakeRedNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFarIntakeRedNew.java
@@ -35,74 +35,62 @@ public class AutonomousFarIntakeRedNew extends LinearOpMode {
 
         robot.driveTrain.gyroTurn(.05, targetAngle);
 
-        robot.jewelSwatter.elbowServo.setPosition(UniversalConstants.jewelElbowForwards);
-        robot.driveTrain.assistedStrafe(-.5, 0);
-        sleep(250);
-        robot.driveTrain.park();
-        robot.jewelSwatter.wristServo.setPosition(UniversalConstants.jewelWristForwards);
         robot.driveTrain.moveToPositionInches(-2.5, .2);
         double startTime;
         switch (vuMark) {
             case LEFT:
-                robot.strafeToJewelSensedDistance(-.2, 5, targetAngle, true);
-                //false parameter gives early exit to function, and doesn't park
-                robot.jewelSwatter.zeroSwatter();
                 startTime = getRuntime();
-                while (opModeIsActive() && (getRuntime() - startTime < 1)) {
-                    robot.driveTrain.assistedStrafe(-.2, 0);
+                while (opModeIsActive() && getRuntime() - startTime < .4) {
+                    robot.driveTrain.assistedStrafe(-.5, 0);
                 }
-                robot.driveTrain.park();
-                robot.jewelSwatter.moveJewelToForwards();
-                sleep(100);
             case CENTER:
-                robot.strafeToJewelSensedDistance(-.2, 5, targetAngle, true);
-                //false parameter gives early exit to function, and doesn't park
-                robot.jewelSwatter.zeroSwatter();
                 startTime = getRuntime();
-                while (opModeIsActive() && (getRuntime() - startTime < 1)) {
-                    robot.driveTrain.assistedStrafe(-.2, 0);
+                while (opModeIsActive() && getRuntime() - startTime < .4) {
+                    robot.driveTrain.assistedStrafe(-.5, 0);
                 }
-                robot.driveTrain.park();
-                robot.jewelSwatter.moveJewelToForwards();
-                sleep(100);
             case RIGHT:
             default:
+                startTime = getRuntime();
+                while (opModeIsActive() && getRuntime() - startTime < .15) {
+                    robot.driveTrain.assistedStrafe(-.5, 0);
+                }
+                robot.driveTrain.park();
+                robot.jewelSwatter.moveJewelToForwards();
+                robot.intakeMecanism.deployFoldoutIntake();
+                sleep(100);
                 robot.strafeToJewelSensedDistance(-.2, 5, targetAngle, true);
                 break;
         }
         robot.jewelSwatter.zeroSwatter();
-        robot.driveTrain.gyroTurn(.1, 10);
-        robot.intakeMecanism.deployFoldoutIntake();
-        robot.intakeMecanism.intake();
-        sleep(250);
         robot.intakeMecanism.outtakeSlowly();
         robot.intakeMecanism.outtake();
 
         robot.driveTrain.moveToInches(5, .25);
-        robot.driveTrain.moveToInches(-7, .5);
+        robot.driveTrain.moveToInches(-5, .25);
+        robot.driveTrain.moveToInches(5, .25);
+        robot.driveTrain.moveToInches(-10, .5);
         robot.intakeMecanism.stopIntake();
         robot.driveTrain.gyroTurn(.25, 90);
         robot.driveTrain.gyroTurn(.07, 90);
         switch (vuMark) {
             case LEFT:
-                robot.driveTrain.moveToPositionInches(5, 1);
                 break;
             case CENTER:
-                robot.driveTrain.moveToPositionInches(10, 1);
+                robot.driveTrain.moveToPositionInches(5, 1);
                 break;
             default:
             case RIGHT:
-                robot.driveTrain.moveToPositionInches(15, 1);
+                robot.driveTrain.moveToPositionInches(10, 1);
                 break;
         }
         robot.driveTrain.gyroTurn(.25, 165);
         robot.intakeMecanism.intake();
         robot.driveTrain.moveToInches(30, 1);
-        robot.driveTrain.moveToInches(-20, 1);
+        robot.driveTrain.moveToInches(-30, 1);
         robot.driveTrain.gyroTurn(.25, 0);
         robot.driveTrain.gyroTurn(.05, 0);
         startTime = getRuntime();
-        while (opModeIsActive() && getRuntime() - startTime < 1) {
+        while (opModeIsActive() && getRuntime() - startTime < .5) {
             robot.driveTrain.assistedStrafe(1, 0);
         }
         robot.driveTrain.park();
