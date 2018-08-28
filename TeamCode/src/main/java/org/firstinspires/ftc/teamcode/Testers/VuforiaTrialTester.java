08cb2cf74686be948256430108024533d27fc095
HiroLZ7 <HiroLZ7@users.noreply.github.com>
2018-08-27 21:16:31 -0500

    Started Vuforia program
    Experiment with Vuforia auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/VuforiaTrialTester.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.Testers;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.RobotModules.Robot;
+
+@Autonomous (name = "Vuforia Trial Demo")
+public class VuforiaTrialTester extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot robot = new Robot(this, true, true, true, DcMotor.ZeroPowerBehavior.BRAKE);
+        robot.addAndUpdateTelemetry("RTG");
+
+    }
+}
