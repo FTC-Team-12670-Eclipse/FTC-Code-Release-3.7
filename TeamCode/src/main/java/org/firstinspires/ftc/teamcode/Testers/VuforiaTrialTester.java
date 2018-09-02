09e4db259cbf31345829c45130f2c10fce04f405
HiroLZ7 <HiroLZ7@users.noreply.github.com>
2018-09-02 14:32:23 -0500

    MAde lots of changes in logic
    Not really working as intended

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/VuforiaTrialTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/VuforiaTrialTester.java
@@ -7,30 +7,39 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.teamcode.RobotModules.Robot;
 
-@Autonomous (name = "Vuforia Trial Demo")
+@Autonomous(name = "Vuforia Trial Demo")
 public class VuforiaTrialTester extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot(this, true, true, true, DcMotor.ZeroPowerBehavior.BRAKE);
         robot.addAndUpdateTelemetry("RTG");
+        waitForStart();
         RelicRecoveryVuMark vuMark;
-        robot.vuforiaRelicRecoveryGetter.activateTrackables();
-        vuMark = robot.vuforiaRelicRecoveryGetter.getPattern();
-
-        vuMark = robot.vuforiaRelicRecoveryGetter.getPattern();
-        if (vuMark == RelicRecoveryVuMark.LEFT){
-            while(vuMark == RelicRecoveryVuMark.LEFT){
-                robot.driveTrain.setAll(.5);
+        while (opModeIsActive()) {
+            robot.vuforiaRelicRecoveryGetter.activateTrackables();
+            vuMark = robot.vuforiaRelicRecoveryGetter.getPattern();
+            if (vuMark == RelicRecoveryVuMark.LEFT) {
+                while (robot.vuforiaRelicRecoveryGetter.getPattern() == RelicRecoveryVuMark.LEFT) {
+                    robot.driveTrain.setAll(.05);
+                    if (robot.vuforiaRelicRecoveryGetter.getPattern() != RelicRecoveryVuMark.LEFT) {
+                        robot.driveTrain.park();
+                        break;
+                    }
+                }
+                robot.driveTrain.park();
             }
-            robot.driveTrain.setAll(0);
-        }
-        vuMark = robot.vuforiaRelicRecoveryGetter.getPattern();
-        if (vuMark == RelicRecoveryVuMark.RIGHT){
-            while(vuMark == RelicRecoveryVuMark.RIGHT){
-                robot.driveTrain.setAll(-.5);
+            vuMark = robot.vuforiaRelicRecoveryGetter.getPattern();
+            if (vuMark == RelicRecoveryVuMark.RIGHT) {
+                while (robot.vuforiaRelicRecoveryGetter.getPattern() == RelicRecoveryVuMark.RIGHT) {
+                    robot.driveTrain.setAll(-.05);
+                    if (robot.vuforiaRelicRecoveryGetter.getPattern() != RelicRecoveryVuMark.RIGHT) {
+                        robot.driveTrain.park();
+                        break;
+                    }
+                }
+                robot.driveTrain.park();
             }
-            robot.driveTrain.setAll(0);
         }
     }
 }
