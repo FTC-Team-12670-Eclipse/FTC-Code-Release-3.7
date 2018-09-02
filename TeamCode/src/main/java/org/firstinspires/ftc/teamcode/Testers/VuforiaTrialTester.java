96e03fcd93be2f2edb73fd6555590686bd4241b3
HiroLZ7 <HiroLZ7@users.noreply.github.com>
2018-09-02 13:05:04 -0500

    New Logical items added to code
    Supposed to respond to vuforia targets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/VuforiaTrialTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testers/VuforiaTrialTester.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.teamcode.RobotModules.Robot;
 
 @Autonomous (name = "Vuforia Trial Demo")
@@ -13,6 +14,23 @@ public class VuforiaTrialTester extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot(this, true, true, true, DcMotor.ZeroPowerBehavior.BRAKE);
         robot.addAndUpdateTelemetry("RTG");
+        RelicRecoveryVuMark vuMark;
+        robot.vuforiaRelicRecoveryGetter.activateTrackables();
+        vuMark = robot.vuforiaRelicRecoveryGetter.getPattern();
 
+        vuMark = robot.vuforiaRelicRecoveryGetter.getPattern();
+        if (vuMark == RelicRecoveryVuMark.LEFT){
+            while(vuMark == RelicRecoveryVuMark.LEFT){
+                robot.driveTrain.setAll(.5);
+            }
+            robot.driveTrain.setAll(0);
+        }
+        vuMark = robot.vuforiaRelicRecoveryGetter.getPattern();
+        if (vuMark == RelicRecoveryVuMark.RIGHT){
+            while(vuMark == RelicRecoveryVuMark.RIGHT){
+                robot.driveTrain.setAll(-.5);
+            }
+            robot.driveTrain.setAll(0);
+        }
     }
 }
